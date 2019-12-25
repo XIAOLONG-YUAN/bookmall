5898804d349f58a544077c0bc74d632351c9c648
yuanxiaolong <xyuan3695@gamil.com>
2019-12-25 14:07:36 +0800

    init

---
+++ yuan-auth/src/main/java/com/yuan/bookmall/YuanAuthApplication.java
@@ -0,0 +1,13 @@
+package com.yuan.bookmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuanAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuanAuthApplication.class, args);
+    }
+
+}
