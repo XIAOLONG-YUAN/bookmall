7d678d5c5711b15c21fa10089f4dfffae3b58ebe
yuanxiaolong <xyuan3695@gamil.com>
2020-01-14 14:31:46 +0800

    添加爬虫模块

--- yuan-order/src/main/java/com/yuan/bookmall/controller/scTest/ScTestController.java
+++ yuan-order/src/main/java/com/yuan/bookmall/controller/scTest/ScTestController.java
@@ -1,6 +1,8 @@
 package com.yuan.bookmall.controller.scTest;
 
 
+import lombok.extern.java.Log;
+import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/scTest")
+@Log
 public class ScTestController {
 
     @GetMapping("/{string}")
     public String test(@PathVariable("string") String string) {
+        log.info("invoke");
         return "Hello Nacos :" + string;
     }
 
